51a78600544e2ebe3b400364ecc1f6dbea98d377
Hackingcrews <40152068+hasnatulhassan@users.noreply.github.com>
2022-12-18 08:37:32 +0500

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
       # URL Shortener #
 
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'Urllover.xyz')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '84746c315238972a5c7b86759068daff1d17757b')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
